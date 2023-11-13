507bc96ddeb2165e3c1367a33ce249df5c85294f
Smyja <akposlive59@gmail.com>
2023-11-13 10:22:45 +0100

    updated chat widget

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import "@mantine/core/styles.css";
 import { MantineProvider } from "@mantine/core";
 import { theme } from "./theme";
-
+import { Demo } from "./components";
 export default function App() {
-  return <MantineProvider theme={theme}>App</MantineProvider>;
+  return <MantineProvider theme={theme}><Demo/></MantineProvider>;
 }
